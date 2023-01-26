575116788c1dc98486e9562100bd905305591c00
safa8787 <amorrisafa7@gmail.com>
2023-01-26 17:16:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieList from './components/MovieList';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import Footer from './components/Footer';
 
 function App() {
+  const [text, SetText] = useState('');
+  console.log("text",text)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar SetText={SetText}/>
+      <MovieList text={text}/>
+      <Footer/>
+     
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h4>© 1999-2022 Safa.com. All rights reserved.</h4>
+        </div>
+    );
+}
+
+export default Footer;
